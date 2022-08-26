b9f2c47d8195e6238515134ce4ccb3c9563e49ee
Jeff-Mwai <jeffmwai3@gmail.com>
2022-08-26 11:52:10 +0300

    calculated the grand total

--- src/app/components/items/items.component.ts
+++ src/app/components/items/items.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { jsPDF } from "jspdf";
 import { ApiCallsService } from 'src/app/services/api-calls.service';
 
 @Component({
@@ -9,18 +10,43 @@ import { ApiCallsService } from 'src/app/services/api-calls.service';
 export class ItemsComponent implements OnInit {
   allData: any = [];
   itemsData: any;
-  todaysDate = new Date 
+  todaysDate = new Date;
+  amount: any 
+  public grandTotal: any;
+  totalAmount: any;
   constructor(private service: ApiCallsService) { }
 
   ngOnInit(): void {
-    this.getItemsData()
+    this.getItemsData();
+    this.grandTotal = this.itemsData.reduce((total: any,myItems: any)=> {
+    
+      //count the items
+      total += myItems.amount
+      return total
+  }, 0)
+  console.log(this.grandTotal)
+
   }
+
+
   getItemsData(){
     this.service.invoiceData.subscribe(res => {
       this.allData = res
       this.itemsData = this.allData.items
-      console.log("second component", this.itemsData)
+      this.itemsData.forEach((element: any) => {
+        Object.assign(element, {amount: element.quantity*element.price});
+      });
+      console.log("itemsDataaaaa",this.itemsData)
+      
     })
-  }
+  };
+
+
+
+
+
+   
+
+
 
 }
--- src/app/services/api-calls.service.ts
+++ src/app/services/api-calls.service.ts
@@ -7,8 +7,43 @@ import { BehaviorSubject } from 'rxjs';
 export class ApiCallsService {
   private invoice = new BehaviorSubject<any>([])
   invoiceData = this.invoice.asObservable()
+  public itemList: any = [];
   constructor() { }
   getInvoiceData(data: any) {
      this.invoice.next(data)
   };
-}
+
+  setItem(item: any){
+    this.itemList.push(...item);
+    this.invoice.next(item);
+  };
+
+  addItem(item: any) {
+    this.itemList.push(item);
+    this.invoice.next(this.itemList);
+    
+    console.log(this.itemList)
+  };
+  
+  // getTotalPrice() :number{
+  //   let grandTotal = 0;
+  //   this.itemList.map((item: any)=> {
+  //     grandTotal += item.amount
+  //   })
+  //   return grandTotal
+  // };
+
+ 
+  // getTotalPrice(): number{
+
+  //   let grandTotal = 0;
+  //   grandTotal = this.itemList.reduce((accumulator: any, a: any) => {
+
+  //     return accumulator + a.amount;
+
+  //   },0);
+
+  //   return grandTotal;
+
+  // }  
+ }
