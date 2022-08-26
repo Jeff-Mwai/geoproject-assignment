6019f5dec3663760821ab85fbc0c6d8354f7ea91
Jeff-Mwai <jeffmwai3@gmail.com>
2022-08-26 12:13:22 +0300

    generated pdf

--- src/app/components/items/items.component.ts
+++ src/app/components/items/items.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { jsPDF } from "jspdf";
 import { ApiCallsService } from 'src/app/services/api-calls.service';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-items',
@@ -8,6 +9,7 @@ import { ApiCallsService } from 'src/app/services/api-calls.service';
   styleUrls: ['./items.component.css']
 })
 export class ItemsComponent implements OnInit {
+  @ViewChild('htmlData') htmlData!: ElementRef;
   allData: any = [];
   itemsData: any;
   todaysDate = new Date;
@@ -24,8 +26,18 @@ export class ItemsComponent implements OnInit {
       total += myItems.amount
       return total
   }, 0)
-  console.log(this.grandTotal)
-
+  }
+  public openPDF(): void {
+    let DATA: any = document.getElementById('htmlData');
+    html2canvas(DATA).then((canvas) => {
+      let fileWidth = 208;
+      let fileHeight = (canvas.height * fileWidth) / canvas.width;
+      const FILEURI = canvas.toDataURL('image/png');
+      let PDF = new jsPDF('p', 'mm', 'a4');
+      let position = 0;
+      PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight);
+      PDF.save('geoProject-invoice.pdf');
+    });
   }
 
 
